91f03602c5051b75209f67c5c6990f098c08d754
ShellMing <shao_ming@outlook.com>
2018-11-14 00:20:59 +0800

    One

--- main.c
+++ main.c
@@ -198,8 +198,89 @@ DN InputStudent(DN phead){
             }
 
         }
+
 }
 
+
+
+void OutputStudentToolOfMark(DN pHead)
+{
+    //定义一个指针用于遍历学生信息
+
+    int count=1;
+    printf("ID   学号  姓名\n");
+
+    while(NULL != pHead)
+    {
+        printf("%d  %s  %s       ", pHead->student.tempId, pHead->student.No, pHead->student.Name);
+        pHead = pHead->DOWN;
+        count++;
+        if(count>5){
+            printf("\n");
+            count=1;
+        }
+
+    }
+
+
+}
+DN InputStudentMark(DN pHead,PMNODE mHead){
+    if(pHead==NULL||mHead==NULL){
+        printf("没有学生或课程数据请录入\n");
+        return 0;
+    }
+    DN pp=pHead->DOWN;
+    DN re=pp;
+   while (1){
+       int i=0;
+       DN p=pHead->DOWN;
+       OutputStudentToolOfMark(p);
+       printf("请输入你要录入成绩的学生序号:\n");
+       scanf("%d",&i);
+       while (p!=NULL){
+           if(p->student.tempId==i){
+               PMNODE copy=MarkNodeCopy(mHead);
+               PMNODE mp=copy->D->D;
+               int mcount=0;
+               float sum=0;
+               while (mp!=NULL){
+                   int flag=1;
+                   while(flag){
+                       printf("请输入%s的%s成绩:",p->student.Name,mp->mark.ClassName);
+                       float mark;
+                       scanf("%f",&mark);
+                       if(mark>=0&&mark<=100){
+                           mp->mark.ClassMark=mark;
+                           sum=sum+mark;
+                           mcount++;
+                           flag=0;
+                       }else{
+                           printf("成绩校验失败！请重新输入！\n");
+                       }
+
+                   }
+                   mp=mp->D;
+               }
+               p->Total=sum;
+               p->Avg=sum/mcount;
+               p->SMN=copy->D->D;
+               //printf("%.1f %.1f",p->Total,p->Avg);
+               //printf("%s,%f",p->SMN->mark.ClassName,p->SMN->mark.ClassMark);
+           }
+           p=p->DOWN;
+       }
+       printf("继续请按Y退出按N(Y?N)");
+       char c;
+       scanf("%s",&c);
+       if(checkExit(c)){
+           continue;
+       } else{
+           return re;
+       }
+   }
+
+
+}
 void OutputStudent(DN pHead)
 {
     //定义一个指针用于遍历学生信息
@@ -234,6 +315,82 @@ void OutputMarkInfo(PMNODE pHead)
 
 
 }
+void OutputMarkDetail(DN pHead){
+    DN p=pHead;
+    while (NULL !=p){
+        printf("ID         学号         姓名         总分         平均分\n");
+        printf("%d          %s          %s          %.1f          %.1f\n",p->student.tempId,p->student.No,p->student.Name,p->Total,p->Avg);
+     PMNODE m=pHead->SMN;
+        while(NULL !=m){
+            printf("%s:          %.1f\n",m->mark.ClassName,m->mark.ClassMark);
+            m=m->D;
+        }
+        p=p->DOWN;
+    }
+}
+
+void DeleteStudent(DN pHead){
+    OutputStudent(pHead);
+    DN d=pHead;
+    printf("请输入你要删除的学生的ID:");
+    int flag=0;
+    scanf("%d",&flag);
+    while(NULL!=d->DOWN){
+        if(flag==d->student.tempId){
+            DN t=d->DOWN;
+            DN h=d->UP;
+            h->DOWN=t;
+            t->UP=h;
+            free(d);
+        }
+        d=d->DOWN;
+    }
+
+    OutputStudent(pHead);
+
+}
+void InsertStudent(DN pHead){
+    OutputStudent(pHead);
+    printf("请输入你要插入的位置！");
+    int flag=0;
+    scanf("%d",&flag);
+    DN pnew=(DN)malloc(sizeof(SNODE));
+    DN temp=pHead;
+    while (NULL!=temp->DOWN){
+        if(temp->student.tempId==flag){
+            SNODE stu;
+            stu.student.tempId=flag;
+            printf("请输入第%d个学生的学号:", flag);
+            scanf("%s", stu.student.No);
+            printf("请输入第%d个学生的姓名:", flag);
+            scanf("%s", stu.student.Name);
+            printf("请输入第%d个学生的性别:", flag);
+            scanf("%s", stu.student.Sex);
+            printf("请输入第%d个学生的班级:", flag);
+            scanf("%s", stu.student.Class);
+            printf("请输入第%d个学生的电话", flag);
+            scanf("%s", stu.student.Tel);
+            pnew->student=stu.student;
+            DN d=temp->DOWN;
+            DN u=temp->UP;
+            u->DOWN=pnew;
+            pnew->UP=u;
+            pnew->DOWN=temp;
+            temp->UP=pnew;
+            while (NULL!=pnew->DOWN){
+                pnew->DOWN->student.tempId++;
+                pnew=pnew->DOWN;
+            }
+
+        }
+        temp=temp->DOWN;
+    }
+
+    OutputStudent(pHead);
+
+
+}
+
 
 
 DN CreateList(void);
@@ -241,14 +398,17 @@ DN InputStudent(DN phead);
 void OutputMarkInfo(PMNODE pHead);
 PMNODE CreateMark(void);
 PMNODE MarkNodeCopy(PMNODE mhead);
+DN InputStudentMark(DN pHead,PMNODE mHead);
+void OutputMarkDetail(DN pHead);
 void main(){
     DN p =CreateList();
     p=InputStudent(p);
+    DeleteStudent(p);
+    InsertStudent(p);
     OutputStudent(p);
     PMNODE pm=CreateMark();
     pm=InputMarkInfo(pm);
     OutputMarkInfo(pm);
-    PMNODE pm2=MarkNodeCopy(pm);
-    OutputMarkInfo(pm2);
-
+    DN pp=InputStudentMark(p,pm);
+    OutputMarkDetail(pp);
 }
