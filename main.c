de43e69d9b87ccd5011e1046e8e4bfab9385ee0f
ShellMing <shao_ming@outlook.com>
2018-11-15 03:09:54 +0800

    One

--- main.c
+++ main.c
@@ -153,15 +153,15 @@ DN InputStudent(DN phead){
                 for(int i=0;;i++){
                     system("cls");//清屏
                     stu.student.tempId=i+1;
-                    printf("请输入第%d个学生的学号:", i+1);
+                    printf("请输入第%d个学生的学号:\n", i+1);
                     scanf("%s", stu.student.No);
-                    printf("请输入第%d个学生的姓名:", i+1);
+                    printf("请输入第%d个学生的姓名:\n", i+1);
                     scanf("%s", stu.student.Name);
-                    printf("请输入第%d个学生的性别:", i+1);
+                    printf("请输入第%d个学生的性别:\n", i+1);
                     scanf("%s", stu.student.Sex);
-                    printf("请输入第%d个学生的班级:", i+1);
+                    printf("请输入第%d个学生的班级:\n", i+1);
                     scanf("%s", stu.student.Class);
-                    printf("请输入第%d个学生的电话", i+1);
+                    printf("请输入第%d个学生的电话\n", i+1);
                     scanf("%s", stu.student.Tel);
                     printf("继续按Y退出按N(Y?N)\n");
                     char s;
@@ -198,7 +198,6 @@ DN InputStudent(DN phead){
             }
 
         }
-
 }
 
 
@@ -246,7 +245,7 @@ DN InputStudentMark(DN pHead,PMNODE mHead){
                while (mp!=NULL){
                    int flag=1;
                    while(flag){
-                       printf("请输入%s的%s成绩:",p->student.Name,mp->mark.ClassName);
+                       printf("请输入%s的%s成绩:\n",p->student.Name,mp->mark.ClassName);
                        float mark;
                        scanf("%f",&mark);
                        if(mark>=0&&mark<=100){
@@ -269,7 +268,7 @@ DN InputStudentMark(DN pHead,PMNODE mHead){
            }
            p=p->DOWN;
        }
-       printf("继续请按Y退出按N(Y?N)");
+       printf("继续请按Y退出按N(Y?N)\n");
        char c;
        scanf("%s",&c);
        if(checkExit(c)){
@@ -315,12 +314,29 @@ void OutputMarkInfo(PMNODE pHead)
 
 
 }
+
+void OutputMark(PMNODE pHead)
+{
+    PMNODE p = pHead->D;
+
+    printf("课程名\n");
+
+    while(NULL != p)
+    {
+        printf("%s  \n", p->mark.ClassName);
+
+        p = p->D;
+
+    }
+
+
+}
 void OutputMarkDetail(DN pHead){
     DN p=pHead;
     while (NULL !=p){
         printf("ID         学号         姓名         总分         平均分\n");
         printf("%d          %s          %s          %.1f          %.1f\n",p->student.tempId,p->student.No,p->student.Name,p->Total,p->Avg);
-     PMNODE m=pHead->SMN;
+     PMNODE m=p->SMN;
         while(NULL !=m){
             printf("%s:          %.1f\n",m->mark.ClassName,m->mark.ClassMark);
             m=m->D;
@@ -328,11 +344,20 @@ void OutputMarkDetail(DN pHead){
         p=p->DOWN;
     }
 }
+void OutputOrderByMarkDetail(DN pHead){
+    DN p=pHead->DOWN;
+    while (NULL !=p){
+        printf("ID         学号         姓名\n");
+        printf("%d          %s          %s\n",p->student.tempId,p->student.No,p->student.Name);
+        p=p->DOWN;
+    }
+}
 
 void DeleteStudent(DN pHead){
     OutputStudent(pHead);
+    DN temp;
     DN d=pHead;
-    printf("请输入你要删除的学生的ID:");
+    printf("请输入你要删除的学生的ID:\n");
     int flag=0;
     scanf("%d",&flag);
     while(NULL!=d->DOWN){
@@ -341,9 +366,14 @@ void DeleteStudent(DN pHead){
             DN h=d->UP;
             h->DOWN=t;
             t->UP=h;
+            temp=t;
             free(d);
         }
         d=d->DOWN;
+    }
+    while (temp!=NULL){
+        temp->student.tempId--;
+        temp=temp->DOWN;
     }
 
     OutputStudent(pHead);
@@ -351,7 +381,7 @@ void DeleteStudent(DN pHead){
 }
 void InsertStudent(DN pHead){
     OutputStudent(pHead);
-    printf("请输入你要插入的位置！");
+    printf("请输入你要插入的位置！\n");
     int flag=0;
     scanf("%d",&flag);
     DN pnew=(DN)malloc(sizeof(SNODE));
@@ -360,15 +390,15 @@ void InsertStudent(DN pHead){
         if(temp->student.tempId==flag){
             SNODE stu;
             stu.student.tempId=flag;
-            printf("请输入第%d个学生的学号:", flag);
+            printf("请输入第%d个学生的学号:\n", flag);
             scanf("%s", stu.student.No);
-            printf("请输入第%d个学生的姓名:", flag);
+            printf("请输入第%d个学生的姓名:\n", flag);
             scanf("%s", stu.student.Name);
-            printf("请输入第%d个学生的性别:", flag);
+            printf("请输入第%d个学生的性别:\n", flag);
             scanf("%s", stu.student.Sex);
-            printf("请输入第%d个学生的班级:", flag);
+            printf("请输入第%d个学生的班级:\n", flag);
             scanf("%s", stu.student.Class);
-            printf("请输入第%d个学生的电话", flag);
+            printf("请输入第%d个学生的电话\n", flag);
             scanf("%s", stu.student.Tel);
             pnew->student=stu.student;
             DN d=temp->DOWN;
@@ -381,15 +411,198 @@ void InsertStudent(DN pHead){
                 pnew->DOWN->student.tempId++;
                 pnew=pnew->DOWN;
             }
-
         }
         temp=temp->DOWN;
     }
-
     OutputStudent(pHead);
+}
+void InsertMark(PMNODE mhead){
 
+    while (NULL!=mhead){
+        if(mhead->D==NULL){
+            PMNODE pnew=(PMNODE)malloc(sizeof(MNODE));
+            MNODE mnode;
+            printf("请输入课程名称:\n");
+            scanf("%s", mnode.mark.ClassName);
+            pnew->mark=mnode.mark;
+            pnew->D=NULL;
+            printf("%s",mhead->mark.ClassName);
+            mhead->D=pnew;
+            printf("继续请按Y退出按N(Y?N)\n");
+            char c;
+            scanf("%s",&c);
+            if(checkExit(c)){
+                continue;
+            } else{
+                break;
+            }
+
+        }
+        mhead=mhead->D;
+    }
+}
+void ChangeStudent(DN pHead){
+    OutputMarkDetail(pHead->DOWN);
+    printf("请输入要修改的学生的ID\n");
+    int tempId;
+    scanf("%d",&tempId);
+    while(pHead!=NULL){
+        if(pHead->student.tempId==tempId){
+            printf("请输入第%d个学生的学号:\n", pHead->student.tempId);
+            scanf("%s", pHead->student.No);
+            printf("请输入第%d个学生的姓名:\n", pHead->student.tempId);
+            scanf("%s", pHead->student.Name);
+            printf("请输入第%d个学生的性别:\n", pHead->student.tempId);
+            scanf("%s", pHead->student.Sex);
+            printf("请输入第%d个学生的班级:\n", pHead->student.tempId);
+            scanf("%s", pHead->student.Class);
+            printf("请输入第%d个学生的电话\n", pHead->student.tempId);
+            scanf("%s", pHead->student.Tel);
+            printf("是否要修改成绩信息确认按Y退出按N(Y?N)\n");
+            char ch;
+            scanf("%s",&ch);
+            if(checkExit(ch)){
+                PMNODE pm=pHead->SMN;
+                int count=0;
+                float sum=0;
+                while (pm!=NULL){
+                    printf("请输入%s的%s成绩\n",pHead->student.Name,pm->mark.ClassName);
+                    scanf("%f",&pm->mark.ClassMark);
+                    count++;
+                    sum=sum+pm->mark.ClassMark;
+                    pm=pm->D;
+                }
+                pHead->Total=sum;
+                pHead->Avg=sum/count;
+                printf("修改完成！\n");
+            }
+        }
+        pHead=pHead->DOWN;
+    }
 
 }
+void ScortByAVG(DN pHead) {
+    DN p, q;//定义两个指针
+
+    SNODE temp;
+
+    for (p = pHead->DOWN; NULL != p; p = p->DOWN) {
+        for (q = p->DOWN; NULL != q; q = q->DOWN) {
+            if (p->Avg < q->Avg)//当前一个学生的语文成绩小于后一个学生的语文成绩时
+            {
+                printf("%.1f<%.1f\n",p->Avg,q->Avg);
+                temp.student = p->student;//交换学生的位置
+                p->student = q->student;
+                q->student = temp.student;
+            }
+        }
+    }
+}
+
+void ScortBySUM(DN pHead) {
+    DN p, q;//定义两个指针
+
+    SNODE temp;
+
+    for (p = pHead->DOWN; NULL != p; p = p->DOWN) {
+        for (q = p->DOWN; NULL != q; q = q->DOWN) {
+            if (p->Total < q->Total)//当前一个学生的语文成绩小于后一个学生的语文成绩时
+            {
+                printf("%.1f<%.1f\n",p->Total,q->Total);
+                temp.student = p->student;//交换学生的位置
+                p->student = q->student;
+                q->student = temp.student;
+            }
+        }
+    }
+}
+
+void ScortByClass(DN pHead, char name[20]) {
+    DN p, q;//定义两个指针
+    PMNODE d,b;
+    SNODE temp;
+    printf("%s", name);
+    for (p = pHead->DOWN; NULL != p; p = p->DOWN) {
+        for (q = p->DOWN; NULL != q; q = q->DOWN) {
+            for(d=p->SMN;NULL!=d;d=d->D){
+                for(b=q->SMN;NULL!=b;b=b->D){
+//                    printf("传入%s",name);
+//                    printf("flag：1\n");
+//                    printf("d的%s\n",d->mark.ClassName);
+//                    printf("b的%s\n",b->mark.ClassName);
+//                    printf("flag：3%d",strcmp(d->mark.ClassName,name));
+//                    printf("flag：4%d",strcmp(b->mark.ClassName,name));
+                    if(strcmp(d->mark.ClassName,name)==0&&strcmp(b->mark.ClassName,name)==0){
+                       // printf("2\n");
+                        if(d->mark.ClassMark < b->mark.ClassMark){
+                           // printf("3\n");
+                           // printf("%.1f < %.1f\n",d->mark.ClassMark,b->mark.ClassMark);
+                            temp.student = p->student;//交换学生的位置
+                            p->student = q->student;
+                            q->student = temp.student;
+
+                        }
+                    }
+
+                }
+            }
+
+
+        }
+
+
+
+    }
+}
+
+
+
+
+void UnityOrderBy(DN pHead){
+    PMNODE pmnode=pHead->DOWN->SMN;
+    int i=0;
+    int k=1;
+    printf("这是一个通用排序，请输入依照什么排序\n");
+    printf("---1.综合排序---\n");
+    printf("---2.科目排序---\n");
+    scanf("%d",&i);
+    switch(i){
+        case 1:
+        {
+            printf("-----1.AVG-----\n");
+            printf("-----2.SUM-----\n");
+            int j=0;
+            scanf("%d",&j);
+            if(j==1){
+                ScortByAVG(pHead);
+                OutputOrderByMarkDetail(pHead);
+            } else if(j==2){
+                ScortBySUM(pHead);
+                OutputOrderByMarkDetail(pHead);
+            }
+        }
+
+            break;
+        case 2:
+        {
+            while (pmnode!=NULL){
+                printf("-----%d.%s-----\n",k,pmnode->mark.ClassName);
+                k++;
+                pmnode=pmnode->D;
+            }
+            printf("请输入要排序的课名\n");
+            char mname [20] ;
+            scanf("%s",mname);
+            ScortByClass(pHead,mname);
+            OutputOrderByMarkDetail(pHead);
+        }
+            break;
+
+
+    }
+
+    }
+
 
 
 
@@ -400,15 +613,20 @@ PMNODE CreateMark(void);
 PMNODE MarkNodeCopy(PMNODE mhead);
 DN InputStudentMark(DN pHead,PMNODE mHead);
 void OutputMarkDetail(DN pHead);
+void ChangeStudent(DN pHead);
 void main(){
     DN p =CreateList();
     p=InputStudent(p);
+    OutputStudent(p);
     DeleteStudent(p);
     InsertStudent(p);
     OutputStudent(p);
     PMNODE pm=CreateMark();
     pm=InputMarkInfo(pm);
-    OutputMarkInfo(pm);
     DN pp=InputStudentMark(p,pm);
     OutputMarkDetail(pp);
+    OutputMark(pm);
+    UnityOrderBy(p);
+    ChangeStudent(p);
+    OutputStudent(p);
 }
